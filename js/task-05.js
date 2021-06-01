1f1d49ff539f041219e6afe7f465615c42f7adb9
sepaev <sepaev@gmail.com>
2021-06-01 23:49:54 +0300

    task7/7

--- js/task-05.js
+++ js/task-05.js
@@ -4,8 +4,18 @@ const refs = {
     nameSpan: document.querySelector('span#name-output'),
 };
 
-refs.nameInput.addEventListener('input', onKeypress);
+refs.nameInput.addEventListener('input', onInput);
 
-function onKeypress() {
-    refs.nameSpan.textContent = (refs.nameInput.value === "")? 'незнакомец': refs.nameInput.value;
+function onInput() {
+    const text = (refs.nameInput.value === "") ? 'незнакомец' : refs.nameInput.value;
+    const textArray = text.split(' ');
+    let fixedTextArray = [];
+    textArray.forEach(element => {
+        if (element !== "") {
+            fixedTextArray.push(element);
+        }
+    });
+    const returnText = fixedTextArray.join(' ');
+    refs.nameSpan.textContent = returnText===""? 'незнакомец' : returnText;
+ 
 }
--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,11 @@
 'use strict';
+const refs = {
+    targetSpan: document.querySelector('span#text'),
+    inputControl: document.querySelector('input#font-size-control'),
+}
+
+refs.inputControl.addEventListener('input', changeSize);
+
+function changeSize() {
+    refs.targetSpan.style.fontSize = `${refs.inputControl.value}px`;
+}
